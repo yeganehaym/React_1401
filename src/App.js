fd6d11fde611e326328d0b251626131322023c98
ali <ali@gmail.com>
2023-02-19 20:09:52 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         React 1401
         </p>
         <a
           className="App-link"
